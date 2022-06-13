63da837b1ea71239bfd6c41844ed1d4079b642c6
rockgard3n <liamcashel@gmail.com>
2022-06-12 20:42:31 -0700

    refactor for dal

--- badbank_fullstack/index.js
+++ badbank_fullstack/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app     = express();
 var cors    = require('cors');
+var dal     = require('./dal.js')
 
 //used to serve static files form public directory 
 app.use(express.static('public'));
@@ -8,13 +9,23 @@ app.use(cors());
 
 // create user account route
 app.get('/account/create/:name/:email/:password', function (req, res) {
-    res.send({
-        name:       req.params.name,
-        email:      req.params.email,
-        password:   req.params.password
-    });
+    dal.create(req.params.name, req.params.email, req.params.password).
+    //using dot then to hit the promise we made in dal.js
+        then((user) => {
+            console.log(user);
+            res.send(user);
+        });
 });
 
+app.get('/account/all', function (req, res) {
+    dal.all().
+        then((docs) => {
+            console.log(docs);
+            res.send(docs);
+        });
+});
+
+/* havent turned these into DAL routes yet
 //login user route
 app.get('/account/login/:email/:password', function (req, res) {
     res.send({
@@ -54,7 +65,7 @@ app.get('/account/balance/:email', function (req, res) {
         email:      req.params.email,
     });
 });
-
+*/
 
 //start our listener for the server
 var port = 3000;
